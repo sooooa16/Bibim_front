204e4e8ae2f0a69c52c68bdd686e3aa65aeb53bf
Kuz-DX <kuzdx8035@gmail.com>
2025-11-03 14:03:56 +0900

    3주차(1차 수정)

--- doyul/nav.js
+++ doyul/nav.js
@@ -1,35 +1,14 @@
 const navClick = (clickedLiElement) => {
-    
-    const allIcons = document.querySelectorAll('.nav-icon');
-    
-    allIcons.forEach(icon => {
-        const currentSrc = icon.src;
-
-        if (currentSrc.includes('Home_active.png')) {
-            icon.src = 'Image/Home_unactive.png';
-        } else if (currentSrc.includes('RegionLife_active.png')) {
-            icon.src = 'Image/RegionLife_unactive.png';
-        } else if (currentSrc.includes('PlaceMarker_active.png')) {
-            icon.src = 'Image/PlaceMarker_unactive.png';
-        } else if (currentSrc.includes('Chat_active.png')) {
-            icon.src = 'Image/Chat_unactive.png';
-        } else if (currentSrc.includes('User_active.png')) {
-            icon.src = 'Image/User_unactive.png';
-        }
-    });
 
     const clickedIcon = clickedLiElement.querySelector('.nav-icon');
     const clickedIconSrc = clickedIcon.src;
 
-    if (clickedIconSrc.includes('Home_unactive.png')) {
-        clickedIcon.src = 'Image/Home_active.png';
-    } else if (clickedIconSrc.includes('RegionLife_unactive.png')) {
-        clickedIcon.src = 'Image/RegionLife_active.png';
-    } else if (clickedIconSrc.includes('PlaceMarker_unactive.png')) {
-        clickedIcon.src = 'Image/PlaceMarker_active.png';
-    } else if (clickedIconSrc.includes('Chat_unactive.png')) {
-        clickedIcon.src = 'Image/Chat_active.png';
-    } else if (clickedIconSrc.includes('User_unactive.png')) {
-        clickedIcon.src = 'Image/User_active.png';
+    if (clickedIconSrc.includes('PlaceMarker_unactive.png')) {
+        return;
     }
+
+    if (clickedIconSrc.includes('_unactive.png')) {
+        clickedIcon.src = clickedIconSrc.replace('_unactive.png', '_active.png');
+    }
+
 };
